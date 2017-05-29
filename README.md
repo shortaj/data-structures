f300ceed94a713811efd86995163c8219a97e59d
Alex <ajshort2010@hotmail.com>
2017-05-28 19:25:54 -0700

    Updated README and setup.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # data-structures
 
-A Single-Linked List is a data structure that has a reference to the head of a class of Nodes and usually the methods to access those Nodes or manipulate them. The Node class will house usually have some set of data and the a reference to the 'Next Node' in the chain. The Single-Linked List class's methods will give you a means of removing/adding Nodes from the list, modifying their data, and giving info on the list itself.
+The Stack in this regard is meant to test how inheritence works in python and how to play with it.
--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 dependencies = ['ipython', 'pytest', 'pytest-watch', 'tox']
 
 setup(
-    name='Linked_List',
-    description='A Single Link-List.',
+    name='Stack',
+    description='A Stack.',
     version='0.5',
     author='Orphelia Yin and Alex Short',
     author_email='ajshort2010@hotmail.com',
-    py_modules=['linked_list'],
+    py_modules=['stack'],
     package_dir={'': 'src'},
     install_requires=dependencies,
-    entry_points={'console_scripts': ['linked_list = linked_list:main']}
+    entry_points={'console_scripts': ['stack = stack:main']}
 )

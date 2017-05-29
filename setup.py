085a88afb355383c65a285be89ba45c7430d482c
Alex Short <ajshort2010@hotmail.com>
2017-05-28 19:12:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 dependencies = ['ipython', 'pytest', 'pytest-watch', 'tox']
 
 setup(
-    name='Queue',
-    description='A Queue Link-List.',
+    name='Double_Link_List',
+    description='A Double Link-List.',
     version='0.5',
     author='Orphelia Yin and Alex Short',
     author_email='ajshort2010@hotmail.com',
-    py_modules=['que_'],
+    py_modules=['dll'],
     package_dir={'': 'src'},
     install_requires=dependencies,
-    entry_points={'console_scripts': ['que_ = que_:main']}
+    entry_points={'console_scripts': ['dll = dll:main']}
 )

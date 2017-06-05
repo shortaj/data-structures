10ad10903159621dd56f24e85cc85ec4b95e8c8f
Alex <ajshort2010@hotmail.com>
2017-06-04 21:59:37 -0700

    test docstring.

--- src/test_binheap.py
+++ src/test_binheap.py
@@ -9,21 +9,21 @@ def import_binheap():
 
 
 def test_Binheap_empty(import_binheap):
-    """."""
+    """Test empty Binheap creation."""
     test_Binheap = import_binheap
     assert test_Binheap.heaplist == []
     assert test_Binheap._length == 0
 
 
 def test_Binheap_size():
-    """."""
+    """Test the Binheap size method."""
     from binheap import Binheap
     test_Binheap = Binheap([1, 2, 3])
     assert test_Binheap._length == 3
     assert test_Binheap.size() == 3
 
 def test_Binheap_pop():
-    """."""
+    """Test the Binheap pop method."""
     from binheap import Binheap
     test_Binheap = Binheap([1, 2, 3])
     assert test_Binheap.pop() == 1
